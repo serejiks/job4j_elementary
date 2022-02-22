dfef5d5a31ad8ae63117e2eb7bd2fe448d83835a
serejiks <sergey.tarasov.99@internet.ru>
2022-02-22 09:25:50 +0500

    Switch - matcher [#334071 #267984] review

--- src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class MultipleSwitchWeek {
     public static int numberOfDay(String name) {
         return switch (name) {
-            case "Понедельник", "Monday" -> 1;
+            case "Понедельник", "Monday", "Понедельник " -> 1;
             case "Вторник", "Tuesday" -> 2;
             case "Среда", "Wednesday" -> 3;
             case "Четверг", "Thursday" -> 4;
