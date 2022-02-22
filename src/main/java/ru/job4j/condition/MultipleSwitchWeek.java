06678ffad7c221fcf66e25c36c5e6145c4a40a53
serejiks <sergey.tarasov.99@internet.ru>
2022-02-22 09:33:42 +0500

    Switch [#334070 #267981] new commit

--- src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -4,7 +4,7 @@ public class MultipleSwitchWeek {
     public static int numberOfDay(String name) {
         return switch (name) {
             case "Понедельник", "Monday", "Понедельник " -> 1;
-            case "Вторник", "Tuesday" -> 2;
+            case "Вторник", "Tuesday", "Вторник " -> 2;
             case "Среда", "Wednesday" -> 3;
             case "Четверг", "Thursday" -> 4;
             case "Пятница", "Friday" -> 5;
