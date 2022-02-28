ea08e670188130e7da035596a0f778c3fd4727f6
serejiks <sergey.tarasov.99@internet.ru>
2022-02-28 08:48:11 +0500

    Крест в псевдографике. [#177182 #269569] review

--- src/main/java/ru/job4j/loop/Slash.java
+++ src/main/java/ru/job4j/loop/Slash.java
@@ -5,7 +5,7 @@ public class Slash {
         for (int row = 0; row < size; row++) {
             for (int cell = 0; cell < size; cell++) {
                 boolean left = row == cell;
-                boolean right = (row + cell - size + 1) == 0;
+                boolean right = row + cell == size - 1;
                 if (left) {
                     System.out.print("0");
                 } else if (right) {
